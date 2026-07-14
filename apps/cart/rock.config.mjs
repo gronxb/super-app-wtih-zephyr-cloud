eb79128a1a0fbd5140bcc9a6650f9a8bad78d1dc
gronxb <gron1gh1@gmail.com>
2026-07-15 08:41:57 +0900

    chore: init project

---
+++ apps/cart/rock.config.mjs
@@ -0,0 +1,11 @@
+import { platformIOS } from '@rock-js/platform-ios';
+import { platformAndroid } from '@rock-js/platform-android';
+import { pluginMetro } from '@rock-js/plugin-metro';
+
+export default {
+  bundler: pluginMetro(),
+  platforms: {
+    ios: platformIOS(),
+    android: platformAndroid(),
+  },
+};
